e8f5bf59abbe4985ad9cb4ba400328637c018fbc
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-04-06 20:05:13 +0530

    Javascript-30 - javascript

---
+++ 10-jsDrumKit/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const playSound = function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
+  if (!audio) {
+    return;
+  }
+  audio.currentTime = 0; // Rewind
+  audio.play();
+  key.classList.add('playing');
+};
+
+window.addEventListener('keydown', playSound);
+
+const removeTransition = function (e) {
+  if (e.propertyName !== 'transform') {
+    return;
+  }
+  this.classList.remove('playing');
+};
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
